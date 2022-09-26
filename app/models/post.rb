2f37e23b68e5b49a2459a5240abdf11413be2a22
koechronix <koechronix@yahoo.com>
2022-09-26 12:35:53 +0300

    Completed basics and customs validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w[Fiction Non-Fiction]}
+
+    validate :clickbait
+
+    def clickbait
+     errors.add(:title, 'title not click-baitey enough') if title == 'True Facts'
+    end
+
 end
